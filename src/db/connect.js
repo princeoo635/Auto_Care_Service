8eed673e64331fe5b33f511db11ea62612df1852
princeoo635 <princeoo635@gmail.com>
2026-01-08 21:57:47 +0530

    feat: add cookie-parser and cors middleware, implement MongoDB connection

---
+++ src/db/connect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB=async ()=>{
+    try {
+        const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URL_LOCAL}/auto_care`)
+        console.log(`MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.error("MongoDB connection Error :", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
