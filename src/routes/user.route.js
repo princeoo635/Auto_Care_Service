263aaad9c7e67f9153160c38fe197ccd5a6e6c4c
princeoo635 <princeoo635@gmail.com>
2026-01-16 16:12:21 +0530

    added routes for login and logout

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,16 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import { registerUser,
+        loginUser,
+        logoutUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router=Router();
 router.route("/register").post(upload.single("profileImage"),registerUser);
-// router.route("/login").post(loginUser)
+router.route("/login").post(loginUser)
+
+//secure routes
+router.route("/logout").get(verifyJWT,logoutUser)
 
 
 
