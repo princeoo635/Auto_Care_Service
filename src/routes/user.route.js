186f57820214182fbbf69293bf95f869c907c63a
princeoo635 <princeoo635@gmail.com>
2026-01-16 15:49:45 +0530

    add routes for user registeration

---
+++ src/routes/user.route.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import {registerUser} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router=Router();
+router.route("/register").post(upload.single("profileImage"),registerUser);
+// router.route("/login").post(loginUser)
+
+
+
+export default router;
