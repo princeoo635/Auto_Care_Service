a3fb632c374f334e38458ea292c80c8ccbbcce54
princeoo635 <princeoo635@gmail.com>
2026-01-16 16:11:16 +0530

    added controller for user login and logout with proper authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,12 @@ import { AsyncHandler } from '../utils/AsyncHandler.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { User } from '../models/user.model.js'
 
+//option
+const option={
+    httpOnly: true,
+    secure: true
+}
+
 //register user
 const registerUser = AsyncHandler( async (req,res) => {
     const { name,email,password,userName,phone,address } = req.body
@@ -45,6 +51,41 @@ const registerUser = AsyncHandler( async (req,res) => {
         new ApiResponse(201,createdUser,"User registered successfully")
     )
 })
+//Login Users
+const loginUser = AsyncHandler( async (req,res) => {
+    const { email,password } = req.body
+    if(!email){
+        throw new ApiError(400,"Email is required.")
+    }
+    const user = await User.findOne({ email })
+    if( !user ){
+        throw new ApiError(404," User does not exists.")
+    }
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if( !isPasswordValid ){
+        throw new ApiError(400,"Password is invalid.")
+    }
+    const accessToken = await user.generateAccessToken()
+    const loggedInUser = await User.findById(user._id).select('-password')
+    res.status(200)
+    .cookie('accesstoken',accessToken,option)
+    .json(
+        new ApiResponse(200,loggedInUser,"user successfully logged in.")
+    )
+})
+//logout
+const logoutUser=AsyncHandler(
+    async (req,res)=>{
+        return res
+        .status(200)
+        .clearCookie("accesstoken",option)
+        .json(new ApiResponse(200,{},"User logged out !!"))
+
+    }
+)
+
 export {
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
 }
