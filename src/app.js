1649e9ce4c46f5ec40b4fa125789103939aa2289
princeoo635 <princeoo635@gmail.com>
2026-01-23 22:48:21 +0530

    added mechanic routes for managing mechanic-related functionalities

--- src/app.js
+++ src/app.js
@@ -15,7 +15,10 @@ app.use(cookieParser())
 
 import userRoutes from "./routes/user.route.js"
 import inventoryRoutes from "./routes/inventory.route.js"
+import mechanicRoutes from "./routes/mechanic.route.js"
+
 app.use("/api/v1/users",userRoutes)
 app.use("/api/v1/inventory",inventoryRoutes)
+app.use("/api/v1/mechanic",mechanicRoutes)
 
 export {app};
