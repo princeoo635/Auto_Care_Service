1f939f2412b39f352ba1612fd5bfe2cba787737f
princeoo635 <princeoo635@gmail.com>
2026-04-06 18:45:23 +0530

    updated MongoDB connection URL to use the correct environment variable

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async ()=>{
     try {
-        const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URL_LOCAL}/Auto_Care_Service`)
+        const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URL}/Auto_Care_Service`)
         console.log(`MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
         
     } catch (error) {
@@ -10,5 +10,5 @@ const connectDB=async ()=>{
         process.exit(1)
     }
 }
-
+ 
 export default connectDB;
