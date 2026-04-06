f7e3203776f6757efef8f31af50a9bf91c8796a8
princeoo635 <princeoo635@gmail.com>
2026-04-06 19:07:55 +0530

    fix: correct import path for Mechanic model in mechanic.controller.js

--- src/controllers/mechanic.controller.js
+++ src/controllers/mechanic.controller.js
@@ -1,7 +1,7 @@
 import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { AsyncHandler } from '../utils/AsyncHandler.js'
-import { Mechanic } from '../models/mechanic.model.js/'
+import { Mechanic } from '../models/mechanic.model.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 
 // add mechaic details
