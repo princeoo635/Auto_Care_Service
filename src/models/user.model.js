2d2d37e08a0151728de983382948b1d5f8cc5d93
Prince kumar <155288006+princeoo635@users.noreply.github.com>
2026-05-27 09:53:51 +0530

    Update user model to include whoEntered
    Replaced 'role' with 'whoEntered' in user model export.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken= function () {
             _id:this._id,
             email:this.email,
             name:this.name,
-            role:this.role,
+            whoEntered:this.whoEntered,
         },
         process.env.ACCESS_TOKEN_SECRET,
         {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
